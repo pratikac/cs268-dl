9eff08ad164bc2e4f25dd5bcc6178fdd55af1fd4
pratik.ac <pratik.ac@gmail.com>
2017-02-22 17:12:50 -0800

    added example for PyTorch

--- README.md
+++ README.md
@@ -12,3 +12,20 @@ th mnist.lua -h
 ```
 
 This is a LeNet-style network for MNIST, it should train to about ~1% testing error in 10 epochs and should get to ~0.75% error around 50 epochs.
+
+
+### Instructions for PyTorch
+
+
+The file ``mnist.py`` contains code to train a CNN on MNIST using [PyTorch](http://pytorch.org).
+
+1. Installing Python on Mac is easiest with conda: [https://www.continuum.io/downloads](https://www.continuum.io/downloads).
+
+2. Install PyTorch for your computer with the appropriate command. For instance, for training with CPU and a Mac with Python 2.7 (should be default for most)
+    ```
+    pip install https://s3.amazonaws.com/pytorch/whl/torch-0.1.9.post2-cp27-none-macosx_10_7_x86_64.whl 
+    pip install torchvision
+    ```
+3. You can now run the code in ``mnist.py`` by doing ``python mnist.py``. It has a few parameters which you can find out by ``python mnist.py -h``.
+
+4. It will download the MNIST dataset and train a convolutional neural network on it. You should expect a test error of about 0.6% after 100 epochs using the parameters in the code (learning rate = 0.1).
---
+++ mnist.py
@@ -0,0 +1,150 @@
+from __future__ import print_function
+import argparse, math, random
+import numpy as np
+import os, pdb, sys, json, subprocess, argparse
+
+import torch as th
+import torch.nn as nn
+from torch.autograd import Variable
+
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+p = argparse.ArgumentParser('')
+p.add_argument('--lr', type=float, default=0.1, help='Learning rate')
+p.add_argument('-b', type=int, default=128, help='Batch size')
+p.add_argument('-B', type=int, default=100, help='Epochs')
+p.add_argument('-g', type=int, default=-1, help='GPU idx. (-1 for CPU)')
+opt = vars(p.parse_args())
+print(opt)
+
+if opt['g'] > -1:
+    print('Using GPU: ', opt['g'])
+    th.cuda.set_device(opt['g'])
+    import torch.backends.cudnn as cudnn
+    cudnn.benchmark = True
+else:
+    print('Using CPU')
+
+kwargs = {}
+if opt['g'] > -1:
+    kwargs = {'num_workers': 1, 'pin_memory': True}
+train_loader = th.utils.data.DataLoader(
+    datasets.MNIST('data', train=True, download=True,
+                    transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=opt['b'], shuffle=True, **kwargs)
+test_loader = th.utils.data.DataLoader(
+    datasets.MNIST('data', train=False,
+                    transform=transforms.Compose([
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=opt['b'], shuffle=False, **kwargs)
+
+class View(nn.Module):
+    def __init__(self,o):
+        super(View, self).__init__()
+        self.o = o
+    def forward(self,x):
+        return x.view(-1, self.o)
+
+class lenet(nn.Module):
+    def __init__(self, opt):
+        super(lenet, self).__init__()
+        self.name = 'lenet'
+        opt['d'] = 0.25
+
+        def convbn(ci,co,ksz,psz,p):
+            return nn.Sequential(
+                nn.Conv2d(ci,co,ksz),
+                nn.ReLU(True),
+                nn.MaxPool2d(psz,stride=psz),
+                nn.BatchNorm2d(co),
+                nn.Dropout(p))
+
+        self.m = nn.Sequential(
+            convbn(1,20,5,3,opt['d']),
+            convbn(20,50,5,2,opt['d']),
+            View(50*2*2),
+            nn.Linear(50*2*2, 500),
+            nn.ReLU(True),
+            nn.Dropout(opt['d']),
+            nn.Linear(500,10))
+
+    def forward(self, x):
+        return self.m(x)
+
+model = lenet(opt)
+criterion = nn.CrossEntropyLoss()
+if opt['g'] > -1:
+    model.cuda()
+    criterion = nn.CrossEntropyLoss().cuda()
+optimizer = optim.SGD(model.parameters(), lr=opt['lr'], momentum=0.9)
+
+print('[Start training]')
+
+def train(e):
+    model.train()
+
+    maxb = len(train_loader)
+    fs, top1 = [], []
+    for i, (x,y) in enumerate(train_loader):
+        if opt['g'] > -1:
+            x, y = x.cuda(), y.cuda()
+        x, yvar  = Variable(x), Variable(y)
+        bsz = x.size(0)
+
+        optimizer.zero_grad()
+        yh = model(x)
+        f = criterion(yh, yvar)
+        f.backward()
+
+        optimizer.step()
+
+        # get the index of the max log-probability
+        pred = yh.data.max(1)[1]
+        acc = pred.eq(y).cpu().sum()/float(bsz)
+        err = 100.*(1-acc)
+
+        fs.append(f.data[0])
+        top1.append(err)
+
+        if i % 100 == 0:
+            print('[%2d][%4d/%4d] %2.4f %2.3f%%'%(e, i, maxb, np.mean(fs), np.mean(top1)))
+    print('Train: [%2d] %2.4f %2.3f%%'%(e, np.mean(fs), np.mean(top1)))
+    print('')
+
+def test(e):
+    model.eval()
+
+    maxb = len(test_loader)
+    fs, top1 = [], []
+    for i, (x,y) in enumerate(test_loader):
+        if opt['g'] > -1:
+            x, y = x.cuda(), y.cuda()
+        x, yvar  = Variable(x, volatile=True), Variable(y, volatile=True)
+        bsz = x.size(0)
+
+        yh = model(x)
+        f = criterion(yh, yvar)
+
+        # get the index of the max log-probability
+        pred = yh.data.max(1)[1]
+        acc = pred.eq(y).cpu().sum()/float(bsz)
+        err = 100.*(1-acc)
+
+        fs.append(f.data[0])
+        top1.append(err)
+
+        if i % 100 == 0:
+            print('[%2d][%4d/%4d] %2.4f %2.3f%%'%(e, i, maxb, np.mean(fs), np.mean(top1)))
+    print('Test: [%2d] %2.4f %2.3f%%'%(e, np.mean(fs), np.mean(top1)))
+    print('')
+
+for e in xrange(opt['B']):
+    train(e)
+    if e % 5 == 0:
+        test(e)
